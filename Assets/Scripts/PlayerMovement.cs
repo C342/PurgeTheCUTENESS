7acce1a0fb79b5197fb283ef45e1fb3c1f145a61
342dev <mbuzurin2024@elam.co.uk>
2026-03-11 10:41:56 +0000

    scriptstuff

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,29 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Transform Player;
+    public float Speed;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    private void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2 (horizontalMovement*Speed, verticalMovement*Speed);
+
+        if (Input.GetKeyDown(KeyCode.A)
+
+        void FixedUpdate()
+        {
+
+        }
+    }
 }
