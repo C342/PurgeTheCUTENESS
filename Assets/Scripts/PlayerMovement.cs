d4f6674cb1e55f2674e97e0ce4c7d6fedea11745
342dev <mbuzurin2024@elam.co.uk>
2026-03-13 12:41:58 +0000

    movementupd

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,19 @@
-using JetBrains.Annotations;
 using System;
-using System.Runtime.CompilerServices;
 using Unity.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem.Layouts;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Horizontal Movement Settings")]
+
     [SerializeField] public float Speed;
+    [SerializeField] public float jumpForce;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] public Transform GroundCheck;
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+    [SerializeField] private LayerMask whatIsGround;
 
     private Rigidbody2D rb;
     private float xAxis;
@@ -31,6 +36,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        rb.linearVelocity = new Vector2 (Speed * xAxis, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(Speed * xAxis, rb.linearVelocity.y);
+    }
+
+    public bool Grounded()
+    {
+        if(Physics2D.Raycast(GroundCheck.position, Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
     }
 }
