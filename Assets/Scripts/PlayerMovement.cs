61d35d6360237e9d08bf58a69a0adfc1546ef4cf
342dev <mbuzurin2024@elam.co.uk>
2026-03-13 12:27:48 +0000

    bettermovementfunction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,36 @@
+using JetBrains.Annotations;
 using System;
+using System.Runtime.CompilerServices;
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem.Layouts;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float Speed;
-    public float maxJumpHeight;
+    [SerializeField] public float Speed;
+
     private Rigidbody2D rb;
-    private Vector2 movement;
+    private float xAxis;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal");
+        GetInputs();
+        Move();
+    }
 
-        rb.linearVelocity = new Vector2(horizontalMovement * Speed, 0);
+    void GetInputs()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
 
-        void FixedUpdate()
-        {
-            movement = new Vector2(horizontalMovement, 0).normalized;
-            rb.linearVelocity = new Vector2(movement.x * Speed, movement.y * Speed) * Time.fixedDeltaTime;
-        }
-        //if(Input.GetKey(KeyCode.Space))
-        //    rb.AddForce (Vector2.up * maxJumpHeight, 0);
+    private void Move()
+    {
+        rb.linearVelocity = new Vector2 (Speed * xAxis, rb.linearVelocity.y);
     }
 }
