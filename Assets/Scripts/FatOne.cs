f27d9dd69c7ea62d166511ea1b68d22fa0815224
342dev <mbuzurin2024@elam.co.uk>
2026-04-27 12:07:35 +0100

    tweaks

---
+++ Assets/Scripts/FatOne.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class FatOne : BaseEnemyClass
+{
+    [SerializeField] private float followDistance = 15f;
+
+    protected void Awake()
+    {
+        base.Awake();
+    }
+
+    void Start()
+    {
+        rb.gravityScale = 12f;
+    }
+
+    protected void Update()
+    {
+        base.Update();
+
+        if (!isRecoiling)
+        {
+            float distanceToPlayer = Vector2.Distance
+                (transform.position,
+                PlayerController.Instance.transform.position);
+
+            if (distanceToPlayer <= followDistance)
+            {
+                transform.position = Vector2.MoveTowards
+                    (transform.position,
+                    new Vector2(PlayerController.Instance.transform.position.x, transform.position.y),
+                    speed * Time.deltaTime);
+            }
+        }
+    }
+
+    public void EnemyHit(float _damageDone, Vector2 _hitDirection, float _hitForce)
+    {
+        base.EnemyHit(_damageDone, _hitDirection, _hitForce);
+    }
+}
