1dbd440bff2e7312a1921864c9a6a32c10b031c0
342dev <mbuzurin2024@elam.co.uk>
2026-05-06 11:50:43 +0100

    h

--- Assets/Scripts/camZoomPoint.cs
+++ Assets/Scripts/camZoomPoint.cs
@@ -1,17 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class camZoomPoint : MonoBehaviour
 {
     [SerializeField] private Camera Cam;
-    [SerializeField] 
+    [SerializeField] private float zoomLevel;
+    [SerializeField] private float zoomDuration;
 
-    void Start()
+    public static CameraFunction Instance;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        StartCoroutine(SmoothZoom());
     }
 
-    void Update()
+    private IEnumerator SmoothZoom()
     {
-        
+        float size = Cam.orthographicSize;
+        float time = 0f;
+
+        while (time < zoomDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / zoomDuration;
+
+            Cam.orthographicSize = Mathf.Lerp(size, zoomLevel, t);
+            yield return null;
+        }
+
+        Cam.orthographicSize = zoomLevel;
     }
 }
